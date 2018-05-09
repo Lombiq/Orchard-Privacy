996bfc9535d329ab869414afe496bfee88b187b8
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-09 23:22:57 +0200

    Adding FeatureNames.cs
    --HG--
    branch : issue/OSOE-22

---
+++ Constants/FeatureNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Lombiq.Privacy.Constants
+{
+    public static class FeatureNames
+    {
+        private const string Prefix = "Lombiq.Privacy.";
+
+        public const string Lombiq_Privacy_Consent_Banner = Prefix + "ConsentBanner";
+        public const string Lombiq_Privacy_Registration_Consent = Prefix + "RegistrationConsent";
+        public const string Lombiq_Privacy_Form_Consent = Prefix + "FormConsent";
+    }
+}
