240370f1fb3f76217a2d5cc1d418a6ea26d99549
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-09 02:31:04 +0200

    Adding ResourceManifest
    --HG--
    branch : issue/OSOE-22

---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.UI.Resources;
+using static Lombiq.Privacy.Constants.ResourceNames;
+
+namespace Lombiq.Privacy
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest.DefineScript(JsCookie).SetUrl("vendors/js.cookie.js");
+
+            manifest.DefineStyle(ConsentBanner).SetUrl("lombiq-privacy-consent-banner.min.css", "lombiq-privacy-consent-banner.css");
+        }
+    }
+}
