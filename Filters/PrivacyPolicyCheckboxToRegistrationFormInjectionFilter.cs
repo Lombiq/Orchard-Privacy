97ce2ac6ce06b9d9ac96fc2967cf9a87af088011
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-09 22:05:12 +0200

    Adding PrivacyPolicyCheckboxToRegistrationFormInjectionFilter and its templates to inject the privacy policy checkbox to the Registration form
    --HG--
    branch : issue/OSOE-22

---
+++ Filters/PrivacyPolicyCheckboxToRegistrationFormInjectionFilter.cs
@@ -0,0 +1,46 @@
+﻿using Lombiq.Privacy.Models;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Layouts.Services;
+using Orchard.Mvc.Filters;
+using System.Web.Mvc;
+
+namespace Lombiq.Privacy.Filters
+{
+    public class PrivacyPolicyCheckboxToRegistrationFormInjectionFilter : FilterProvider, IResultFilter
+    {
+        private readonly IOrchardServices _orchardServices;
+        private readonly ICurrentControllerAccessor _currentControllerAccessor;
+
+
+        public PrivacyPolicyCheckboxToRegistrationFormInjectionFilter(
+            IOrchardServices orchardServices,
+            ICurrentControllerAccessor currentControllerAccessor)
+        {
+            _orchardServices = orchardServices;
+            _currentControllerAccessor = currentControllerAccessor;
+        }
+
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) { }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (Orchard.UI.Admin.AdminFilter.IsApplied(filterContext.RequestContext)) return;
+
+            if (filterContext.Result is PartialViewResult) return;
+
+            var workContext = _orchardServices.WorkContext;
+
+            if (!workContext.CurrentSite.As<PrivacySettingsPart>().EnablePrivacyCheckboxOnRegistrationPage) return;
+
+            if (_currentControllerAccessor.CurrentController == null) return;
+            
+            if (workContext.HttpContext.Request.Path == 
+                $"/{nameof(Orchard.Users)}/Account/{nameof(Orchard.Users.Controllers.AccountController.Register)}")
+            {
+                workContext.Layout.Content.Add(_orchardServices.New.Lombiq_Privacy_RegistrationCheckbox(), "before");
+            }
+        }
+    }
+}
