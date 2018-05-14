5e564d54852c3b076d9f03ec58ab45f7ffa6d2a5
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-14 20:50:48 +0200

    Adding handler and driver for ConsentCheckboxPart
    --HG--
    branch : issue/OSOE-22

---
+++ Drivers/ConsentCheckboxPartDriver.cs
@@ -0,0 +1,27 @@
+﻿using Lombiq.Privacy.Models;
+using Orchard;
+using Orchard.ContentManagement.Drivers;
+
+namespace Lombiq.Privacy.Drivers
+{
+    public class ConsentCheckboxPartDriver : ContentPartDriver<ConsentCheckboxPart>
+    {
+        private readonly IWorkContextAccessor _wca;
+
+
+        public ConsentCheckboxPartDriver(IWorkContextAccessor wca)
+        {
+            _wca = wca;
+        }
+
+
+        protected override DriverResult Editor(ConsentCheckboxPart part, dynamic shapeHelper) =>
+            ContentShape("Parts_ConsentCheckbox_Edit", () =>
+            {
+                if (_wca.GetContext()?.CurrentUser == null)
+                    return shapeHelper.Parts_ConsentCheckbox_Edit();
+
+                return null;
+            });
+    }
+}
---
+++ Handlers/ConsentCheckboxPartHandler.cs
@@ -0,0 +1,9 @@
+﻿using Orchard.ContentManagement.Handlers;
+
+namespace Lombiq.Privacy.Handlers
+{
+    public class ConsentCheckboxPartHandler : ContentHandler
+    {
+        public ConsentCheckboxPartHandler() { }
+    }
+}
