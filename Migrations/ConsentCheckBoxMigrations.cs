26c313afa831fd6555aee0f2c17168a5d20b4f89
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-14 20:49:27 +0200

    Adding ConsentCheckboxPart and its migration
    --HG--
    branch : issue/OSOE-22

---
+++ Migrations/ConsentCheckBoxMigrations.cs
@@ -0,0 +1,19 @@
+﻿using Lombiq.Privacy.Models;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Lombiq.Privacy.Migrations
+{
+    public class ConsentCheckBoxMigrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterPartDefinition(nameof(ConsentCheckboxPart), part => part
+                .WithDescription("Attach to a content type to provide Privacy Consent checkbox.")
+                .Attachable());
+
+            return 1;
+        }
+    }
+}
---
+++ Models/ConsentCheckboxPart.cs
@@ -0,0 +1,6 @@
+﻿using Orchard.ContentManagement;
+
+namespace Lombiq.Privacy.Models
+{
+    public class ConsentCheckboxPart : ContentPart { }
+}
