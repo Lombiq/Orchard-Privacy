dc59c9e2deab2c27c59b09b4b08d67156fc7c0bd
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-15 02:16:08 +0200

    Adding ConsentCheckbox FormElement
    --HG--
    branch : issue/OSOE-22

---
+++ Elements/ConsentCheckbox.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.Localization;
+
+namespace Orchard.DynamicForms.Elements
+{
+    public class ConsentCheckbox : FormElement
+    {
+        public override string Name => "ConsentCheckbox";
+        public override string Category => "Forms";
+        public override LocalizedString Description => T("Add a consent checkbox to your layout.");
+        public override LocalizedString DisplayText => T("Consent Checkbox");
+        public override bool HasEditor => false;
+        public override string ToolboxIcon => "\uf046";
+    }
+}
