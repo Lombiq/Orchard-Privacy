46a0f3ca61e24d08719cb194355daf46ae015b47
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-15 02:25:49 +0200

    Documenting ICookieService
    --HG--
    branch : issue/OSOE-22

--- Services/ICookieService.cs
+++ Services/ICookieService.cs
@@ -2,9 +2,20 @@
 
 namespace Lombiq.Privacy.Services
 {
+    /// <summary>
+    /// Managing consent-related cookies.
+    /// </summary>
     public interface ICookieService : IDependency
     {
+        /// <summary>
+        /// Check if the user has consent.
+        /// </summary>
+        /// <returns>True if the user accepts the privacy policy.</returns>
         bool UserHasConsent();
+
+        /// <summary>
+        /// Creating the consent cookie for the current user.
+        /// </summary>
         void SetConsentCookie();
     }
 }
