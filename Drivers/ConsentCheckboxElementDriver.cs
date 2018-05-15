050cf8289f4cecd8d67d84fa28e05128d86cc945
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-15 02:16:24 +0200

    Adding driver for ConsentCheckbox
    --HG--
    branch : issue/OSOE-22

---
+++ Drivers/ConsentCheckboxElementDriver.cs
@@ -0,0 +1,29 @@
+﻿using Orchard.DynamicForms.Elements;
+using Orchard.Layouts.Framework.Display;
+using Orchard.Layouts.Framework.Drivers;
+using Orchard.Layouts.Helpers;
+using Orchard.Layouts.Services;
+using Orchard.Tokens;
+
+namespace Lombiq.Privacy.Drivers
+{
+    public class ConsentCheckboxElementDriver : FormsElementDriver<ConsentCheckbox>
+    {
+        private readonly ITokenizer _tokenizer;
+
+
+        public ConsentCheckboxElementDriver(IFormsBasedElementServices formsServices, ITokenizer tokenizer)
+            : base(formsServices)
+        {
+            _tokenizer = tokenizer;
+        }
+
+
+        protected override void OnDisplaying(ConsentCheckbox element, ElementDisplayingContext context)
+        {
+            context.ElementShape.ProcessedName = _tokenizer.Replace("ConsentCheckbox", context.GetTokenData());
+            // https://github.com/OrchardCMS/Orchard/issues/4123
+            context.ElementShape.ProcessedValue = _tokenizer.Replace("false", context.GetTokenData());
+        }
+    }
+}
