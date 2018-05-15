26cb8fb3169e5893602a9a3289901aa55f28fa55
Gábor Domonkos <gabor.domonkos@lombiq.com>
2018-05-15 02:16:46 +0200

    Adding validator for ConsentCheckbox
    --HG--
    branch : issue/OSOE-22

---
+++ Validators/ConsentCheckboxValidator.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.DynamicForms.Elements;
+using Orchard.DynamicForms.Services;
+using Orchard.DynamicForms.ValidationRules;
+using System.Collections.Generic;
+
+namespace Lombiq.Privacy.Validators
+{
+    public class ConsentCheckboxValidator : ElementValidator<ConsentCheckbox>
+    {
+        private readonly IValidationRuleFactory _validationRuleFactory;
+
+
+        public ConsentCheckboxValidator(IValidationRuleFactory validationRuleFactory)
+        {
+            _validationRuleFactory = validationRuleFactory;
+        }
+
+
+        protected override IEnumerable<IValidationRule> GetValidationRules(ConsentCheckbox element)
+        {
+            yield return _validationRuleFactory.Create<Mandatory>("You have to agree to the privacy policy.");
+        }
+    }
+}
