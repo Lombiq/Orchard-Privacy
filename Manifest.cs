e3007aed00a6809a25bcc450e9b17a3b0760e60e
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2022-02-15 21:57:01 +0100

    Updating version number

--- Manifest.cs
+++ Manifest.cs
@@ -5,7 +5,7 @@ using static Lombiq.Privacy.Constants.FeatureNames;
     Name = "Lombiq Privacy",
     Author = "Lombiq Technologies",
     Website = "https://lombiq.com",
-    Version = "1.0.0"
+    Version = "2.0.0"
 )]
 
 [assembly: Feature(
