13f66bb55f272396dfdc36bfeb757f0278290a1e
József Horváth <info@ministro.hu>
2022-05-18 22:26:35 +0200

    Escape action URL parameter in workflowHttpEventUrlResolve script method

--- Lombiq.Privacy.Samples/Scripting/WorkflowHelperMethodProvider.cs
+++ Lombiq.Privacy.Samples/Scripting/WorkflowHelperMethodProvider.cs
@@ -10,31 +10,30 @@ namespace Lombiq.Privacy.Samples.Scripting;
 
 public class WorkflowHelperMethodProvider : IGlobalMethodProvider
 {
-    private readonly ISecurityTokenService _securityTokenService;
-
     private readonly GlobalMethod _workflowHttpEventUrlResolve;
 
-    public WorkflowHelperMethodProvider(ISecurityTokenService securityTokenService)
-    {
-        _securityTokenService = securityTokenService;
-
+    public WorkflowHelperMethodProvider() =>
         _workflowHttpEventUrlResolve = new GlobalMethod
         {
             Name = "workflowHttpEventUrlResolve",
             Method = serviceProvider => (Func<string, string, int, object>)((workflowTypeId, activityId, tokenLifeSpan) =>
             {
+                var securityTokenService = serviceProvider.GetRequiredService<ISecurityTokenService>();
                 var workflowTypeStore = serviceProvider.GetRequiredService<IWorkflowTypeStore>();
 
                 var workflowType = workflowTypeStore.GetAsync(workflowTypeId).GetAwaiter().GetResult();
-                var token = _securityTokenService.CreateToken(
+                var token = securityTokenService.CreateToken(
                     new WorkflowPayload(workflowType.WorkflowTypeId, activityId),
                     TimeSpan.FromDays(
                         tokenLifeSpan == 0 ? HttpWorkflowController.NoExpiryTokenLifespan : tokenLifeSpan));
 
-                return $"/workflows/Invoke?token={token}";
+                // LinkGenerator.GetPathByAction(...) and UrlHelper.Action(...) not resolves url for
+                // HttpWorkflowController.Invoke action.
+                // Any ideas are welcome.
+
+                return $"/workflows/Invoke?token={Uri.EscapeDataString(token)}";
             }),
         };
-    }
 
     public IEnumerable<GlobalMethod> GetMethods()
     {
